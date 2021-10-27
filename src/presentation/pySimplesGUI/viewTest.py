5f0068c443e3a2d4fcf9faf64544765ca136ac8c
caous <caous.g@gmail.com>
2021-10-27 12:59:38 -0300

    Lab Python

---
+++ src/presentation/pySimplesGUI/viewTest.py
@@ -0,0 +1,22 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+# layout
+sg.theme('Reddit')
+
+inputs = [
+    [sg.Text('Usuário'), sg.Input(key='usuario', size=(20, 1))],
+    [sg.Text('Senha'), sg.Input(key='password',
+                                password_char='*', size=(20, 1))],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
+# janela
+janela = sg.Window('Tela de Login', inputs)
+# ler eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == "admin" and valores['password'] == "123":
+            print("Bem-vindo")
